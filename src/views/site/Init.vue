01801e98be9913f586e407e304e0225739d39f39
caixiang <sfeer@sina.com>
2020-02-18 21:24:32 +0800

    自定义网站选取模版功能

---
+++ src/views/site/Init.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="sbox-init">
+    <div class="row-title">常用</div>
+    <div class="row">
+      <div class="item" v-for="t in templates" :key="t.id">
+        <img :src="t.img" :alt="t.name">
+        <div class="item-title">{{t.name}}</div>
+      </div>
+    </div>
+    <div class="row-title">推荐</div>
+    <div class="row">
+      <div class="item" v-for="t in templates" :key="t.id">
+        <img :src="t.img" :alt="t.name">
+        <div class="item-title">{{t.name}}</div>
+      </div>
+    </div>
+    <div class="row-title">门户</div>
+    <div class="row">
+      <div class="item" v-for="t in templates" :key="t.id">
+        <img :src="t.img" :alt="t.name">
+        <div class="item-title">{{t.name}}</div>
+      </div>
+    </div>
+    <div class="row-title">公文 </div>
+    <div class="row">
+      <div class="item" v-for="t in templates" :key="t.id">
+        <img :src="t.img" :alt="t.name">
+        <div class="item-title">{{t.name}}</div>
+      </div>
+    </div>
+  </div>
+</template>

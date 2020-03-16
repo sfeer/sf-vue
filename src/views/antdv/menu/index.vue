d2d2139a85ba3217d0c4ef5107a3ef0e1e9562a4
Sfeer <sfeer@sina.com>
2020-03-16 16:53:11 +0800

    菜单

--- src/views/antdv/menu/index.vue
+++ src/views/antdv/menu/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <a-menu @click="onSelect">
-    <a-menu-item v-for="menu in menus" :key="menu.key" :data="menu.data">{{item.title}}</a-menu-item>
-  </a-menu>
+  <div class="main">
+    <div class="box box1">
+      <a-menu @click="onSelect" mode="horizontal" :selectable="false">
+        <a-menu-item v-for="menu in menus" :key="menu.key" :data="menu.data">{{menu.title}}</a-menu-item>
+        <a-sub-menu :key="'x'" title="子菜单">
+          <a-menu-item :key="'x1'">子菜单项1</a-menu-item>
+          <a-menu-item :key="'x2'">子菜单项2</a-menu-item>
+          <a-menu-item :key="'x3'">子菜单项3</a-menu-item>
+          <a-menu-item :key="'x4'">子菜单项4</a-menu-item>
+          <a-menu-item :key="'x5'">子菜单项5</a-menu-item>
+          <a-menu-item :key="'x6'">子菜单项6</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </div>
+    <div class="box box2"></div>
+  </div>
 </template>
 
 <script>
+  import {Menu} from 'ant-design-vue'
+
   export default {
     data() {
       return {
@@ -15,6 +30,13 @@
       }
     },
 
+    components: {
+      [Menu.name]: Menu,
+      [Menu.Item.name]: Menu.Item,
+      [Menu.SubMenu.name]: Menu.SubMenu,
+      [Menu.Divider.name]: Menu.Divider,
+    },
+
     methods: {
       onSelect({item, key}) {
         console.log(item, key)
@@ -22,3 +44,24 @@
     }
   }
 </script>
+
+<style lang="less" scoped>
+  .box {
+    position: absolute;
+    width: 400px;
+    padding-top: 152px;
+    height: 200px;
+  }
+
+  .box {
+    top: 100px;
+    left: 200px;
+    background: #8fd4ff;
+  }
+
+  .box2 {
+    top: 300px;
+    left: 200px;
+    background: #ff7968;
+  }
+</style>

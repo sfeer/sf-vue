f577fb0af799701efa5ddbd7ac59ee3a9f45b318
caixiang <sfeer@sina.com>
2020-03-30 21:24:05 +0800

    TBox

---
+++ src/components/TBox/TBox.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div>在SBox的基础上</div>
+    <div>#1 去除分割线调节</div>
+    <div>#2 在每个showBoxs的右下角显示可拖拽的图标</div>
+    <div>#3 在父节点有空闲空间时可以变大、也可以变小（不能小于最小高宽）</div>
+    <div>#4 空闲的高宽，可以在递归后保存在每个box属性中（freeW，freeH）</div>
+  </div>
+</template>

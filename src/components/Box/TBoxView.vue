1c51c494ef9bc504f7a0bf246c89adc4db3c60e9
Sfeer <sfeer@sina.com>
2020-04-07 16:47:40 +0800

    TBox

--- src/components/Box/TBoxView.vue
+++ src/components/Box/TBoxView.vue
@@ -53,21 +53,38 @@
         }
       },
 
-      // 自动改变区域宽度（递归），等比例缩放
+      // 自动改变区域宽度（递归）
       autoWidthBox(box, width) {
-        const vv = width / box.w, childs = this.boxs.filter(b => b.parent === box.id)
-        if (box.mode === 'row') {
-          childs.forEach(bb => {
-            bb.x = box.x
-            this.autoWidthBox(bb, bb.w * vv)
-          })
-        } else if (box.mode === 'col') {
-          childs.forEach(bb => {
-            bb.x = bb.x * vv
-            this.autoWidthBox(bb, bb.w * vv)
-          })
+        if (width !== box.w) {
+          const childs = this.boxs.filter(b => b.parent === box.id)
+          if (box.lw) {
+            // 锁定宽度
+            if (width < box.lw) {
+              width = box.lw
+            }
+            const add = box.x + (width - box.lw) / 2 - box.lx
+            childs.forEach(b => {
+              this.autoXBox(b, add)
+            })
+            box.lx += add
+          } else {
+            // 等比例缩放
+            const vv = width / box.w
+            if (box.mode === 'row') {
+              childs.forEach(bb => {
+                bb.x = box.x
+                this.autoWidthBox(bb, bb.w * vv)
+              })
+            } else if (box.mode === 'col') {
+              childs.forEach(bb => {
+                bb.x = bb.x * vv
+                this.autoWidthBox(bb, bb.w * vv)
+              })
+            }
+          }
+
+          box.w = width
         }
-        box.w = width
       },
 
       handleResize() {
